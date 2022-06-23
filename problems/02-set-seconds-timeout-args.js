d9f2d3813f775cb382d3027f448a4f3b49ccd8ae
evan <ebmorgansb@gmail.com>
2022-06-23 12:41:16 -0700

    up to 2

--- problems/02-set-seconds-timeout-args.js
+++ problems/02-set-seconds-timeout-args.js
@@ -20,10 +20,18 @@ setSecondsTimeoutArgs(printSum, 0.25, 5, 1, 4); // should print '10' after 250ms
 setSecondsTimeoutArgs(function(arg1, arg2) {
     console.log(arg1 + '-' + arg2);
 }, 0.7, 'hello', 'world'); // should print 'hello-world' after 700ms
+
+The `setSecondsTimeout` you wrote was cool, but it's missing some functionality.
+The built-in `setTimeout` can also accept any additional number of arguments that
+will be passed to the callback.
+
+Write a function, `setSecondsTimeoutArgs`, that accepts a callback, a delay
+in seconds, and any number of additional arguments. The function should call the
+callback with the additional arguments after the specified delay.
 ***********************************************************************/
 
 function setSecondsTimeoutArgs(cb, delayInSeconds, ...args) {
-  // Your code here
+  return setTimeout(cb, delayInSeconds*1000, ...args)
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
